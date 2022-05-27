8987beece08150b1176e8e6460797402e4302887
paul <paul@lord.localdomain>
2022-05-28 02:49:45 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+
+const app = express()
+const port = process.env.PORT || 4000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, API!')
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}!`)
+})
