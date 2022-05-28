c1d990b95cd16f934bab50806095b9f9f5e01ee4
paul <paul@lord.localdomain>
2022-05-28 04:19:10 +0300

    Created GraphQL API with using ApolloServer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
 
 
-const app = express()
 const port = process.env.PORT || 4000
 
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`
 
-app.get('/', (req, res) => {
-    res.send('Hello, API!')
-})
+const resolvers = {
+    Query: {
+        hello: () => 'Hello, GraphQL API!'
+    }
+}
 
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}!`)
-})
+async function startApolloServer(typeDefs, resolvers, port) {
+    const app = express()
+    const server = new ApolloServer({ typeDefs, resolvers, 
+    csrfPrevention: true, })
+    await server.start()
+    server.applyMiddleware({ app, path: '/api' })
+    app.listen({ port }, () =>
+        console.log(
+            `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+        ))
+}
+
+startApolloServer(typeDefs, resolvers, port)
